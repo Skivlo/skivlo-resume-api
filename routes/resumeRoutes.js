c73b2afbf6ea39282b5554a56681bd154026c567
Skivlo <official.skivlo@gmail.com>
2026-05-08 01:14:59 +0530

    Connect route with controller

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -2,18 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/generate-resume", async (req, res) => {
-  try {
-    res.status(200).json({
-      success: true,
-      message: "Resume route working"
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server error"
-    });
-  }
-});
+const {
+    generateResume
+} = require("../controllers/resumeController");
+
+router.post("/generate-resume", generateResume);
 
 module.exports = router;
