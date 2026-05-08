d378fbe530758287d5b93d392e662383ba0e7bbf
Skivlo <official.skivlo@gmail.com>
2026-05-08 20:52:51 +0530

    Create razorpay.js

---
+++ config/razorpay.js
@@ -0,0 +1,10 @@
+const Razorpay = require("razorpay");
+
+const env = require("./env");
+
+const razorpay = new Razorpay({
+    key_id: env.RAZORPAY_KEY_ID,
+    key_secret: env.RAZORPAY_SECRET
+});
+
+module.exports = razorpay;
