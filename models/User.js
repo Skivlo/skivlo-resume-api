be7189d915541b804cd2a579cd46190f9d36b327
Skivlo <official.skivlo@gmail.com>
2026-05-11 01:08:05 +0530

    Upgrade user model for  final MVP buisness logic

--- models/User.js
+++ models/User.js
@@ -3,38 +3,99 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 
     name: {
+
         type: String,
+
         required: true
+
     },
 
     email: {
+
         type: String,
+
         required: true,
+
         unique: true
+
     },
 
     plan: {
+
         type: String,
+
         default: "free"
+
     },
 
     resumesUsed: {
+
         type: Number,
+
         default: 0
+
     },
 
-    premiumTemplatesUnlocked: {
+    dailyResumeCount: {
+
         type: Number,
+
         default: 0
+
+    },
+
+    lastResumeDate: {
+
+        type: Date,
+
+        default: null
+
+    },
+
+    unlockedTemplates: {
+
+        type: [String],
+
+        default: []
+
+    },
+
+    boostPurchased: {
+
+        type: Boolean,
+
+        default: false
+
+    },
+
+    adsWatched: {
+
+        type: Number,
+
+        default: 0
+
+    },
+
+    watermarkAdsWatched: {
+
+        type: Number,
+
+        default: 0
+
     },
 
     referralCount: {
+
         type: Number,
+
         default: 0
+
     }
 
 }, {
+
     timestamps: true
+
 });
 
 module.exports = mongoose.model("User", userSchema);
