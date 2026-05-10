55e8d62cce00cbb8eab4704ccdd8c21c47b8dc8d
Skivlo <official.skivlo@gmail.com>
2026-05-10 14:56:03 +0530

    Upgrade authentication controller with JWT login system

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,14 @@ const asyncHandler = require("../utils/asyncHandler");
 
 const User = require("../models/User");
 
+const generateToken = require("../utils/generateToken");
+
+/*
+========================
+     REGISTER USER
+========================
+*/
+
 const registerUser = asyncHandler(async (req, res) => {
 
     const {
@@ -28,11 +36,43 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
         success: true,
         message: "User registered successfully",
+        token: generateToken(user._id),
+        user
+    });
+
+});
+
+/*
+========================
+       LOGIN USER
+========================
+*/
+
+const loginUser = asyncHandler(async (req, res) => {
+
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+
+        return res.status(404).json({
+            success: false,
+            message: "User not found"
+        });
+
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Login successful",
+        token: generateToken(user._id),
         user
     });
 
 });
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
