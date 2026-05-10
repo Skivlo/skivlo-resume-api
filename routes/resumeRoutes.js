6098ada2f1f448891d4b48aa8e3b1c4d1525b7bd
Skivlo <official.skivlo@gmail.com>
2026-05-10 15:11:44 +0530

    Update resumeRoutes.js

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,22 +1,27 @@
 const express = require("express");
 
-const router = express.Router();
-
 const {
     generateResume
 } = require("../controllers/resumeController");
 
 const validateResumeInput = require("../middlewares/validationMiddleware");
 
+const {
+    checkFreeUsageLimit
+} = require("../middlewares/usageMiddleware");
+
+const router = express.Router();
+
 /*
 ========================
-    RESUME ROUTES
+     RESUME ROUTES
 ========================
 */
 
 router.post(
     "/generate",
     validateResumeInput,
+    checkFreeUsageLimit,
     generateResume
 );
 
